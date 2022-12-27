a30376cb277458bc914d58aaff5e700162f3e776
Gonajaime <jaime.gonzalez@bootcamp-upgrade.com>
2022-12-27 14:10:10 +0100

    third

--- Pokedex/Pokedex.js
+++ Pokedex/Pokedex.js
@@ -4,6 +4,7 @@ const getPokemon = async (id) => {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   const responseJson = await response.json();
   myArray = [...myArray, responseJson];
+  console.log(myArray);
 };
 const init = async () => {
   for (let index = 1; index <= 151; index++) {
@@ -16,17 +17,33 @@ const init = async () => {
 const showPoke = (myArray) => {
   const myDiv$$ = document.querySelector(".pokemon");
   myDiv$$.innerHTML = "";
-  for (const pokemon of myArray) {  
+  for (const pokemon of myArray) {
     const newDiv$$ = document.createElement("div");
+    const cardBack = document.createElement("div");
+    const cardFront = document.createElement("div");
+    cardBack.className = "cardB";
+    cardFront.className = "cardA";
     newDiv$$.className = "boxpoke";
-    newDiv$$.innerHTML = `
+    cardFront.innerHTML = `
             <h4 class = "pokename">${pokemon.name}</h4>
-            <p class="poketype">${pokemon.types.map((type)=>type.type.name).join("/")}</p>
-            <img class= "pokeimg"src="${pokemon.sprites.other.home.front_default}" /> 
+            <p class="poketype">${pokemon.types
+              .map((type) => type.type.name)
+              .join(" / ")}</p>
+            <img class= "pokeimg"src="${
+              pokemon.sprites.other.home.front_default
+            }" /> 
             <h4 class = "pokenumber">${pokemon.id}</h4> 
-            <h4 class= "pokedata"> ||Peso: ${pokemon.weight}kg  ||
-            Altura: ${pokemon.height} m||</h4> `;
+             `;
+    cardBack.innerHTML = `<img class= "imgBack"src="${
+      pokemon.sprites.other.dream_world.front_default}"/>
+      <h4 class= "pokedata"> ||Peso: ${pokemon.weight}kg  ||
+    Altura: ${pokemon.height} m|| </h4> 
+    <p class="pokeBase_Stats">${pokemon.stats.map((stat)=> stat.stat.name + " = " + stat.base_stat).join(" / ")}</p>
+    `;
+
+    newDiv$$.appendChild(cardBack);
     myDiv$$.appendChild(newDiv$$);
+    newDiv$$.appendChild(cardFront);
   }
 };
 const printPokemon = () => {
@@ -37,7 +54,7 @@ const printPokemon = () => {
 };
 const searchPoke = (filtro, myArray) => {
   const filtering = myArray.filter((pokemon) =>
-    pokemon.name.toLowerCase().includes(filtro.toLowerCase()) 
+    pokemon.name.toLowerCase().includes(filtro.toLowerCase())
   );
   showPoke(filtering);
 };
